af77e4e5cb637e5c563afeeec1c2a81784396dd2
Graham Watson <graham@phantomwatson.com>
2015-12-27 19:33:05 -0500

    Add __construct()

--- src/EtherMarkov/EtherMarkovChain.php
+++ src/EtherMarkov/EtherMarkovChain.php
@@ -7,4 +7,19 @@ class EtherMarkovChain
     protected $words;
     protected $lcWords;
     protected $caseInsensitive;
+    
+    /**
+     * @param string $sample
+     * @param int $chainLength
+     * @param boolean $caseInsensitive
+     */
+    public function __construct($sample, $chainLength = 2, $caseInsensitive = true)
+    {
+        $this->sample = $sample;
+        $this->caseInsensitive = $caseInsensitive;
+        $this->words = $this->splitText($this->sample, $chainLength);
+        if ($this->caseInsensitive) {
+            $this->lcWords = $this->splitText(strtolower($this->sample), $chainLength);
+        }
+    }
 }
